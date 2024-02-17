05c92b87444ef34fb0c64f52b240d45d4e078c47
jafarmh <jafar.m@yahoo.com>
2024-02-17 21:47:34 +0330

    add padding

--- app/src/layout/sidebar/MenuItem.tsx
+++ app/src/layout/sidebar/MenuItem.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const MenuItem = ({icon,title}:MenuItemProps) => {
     return (
         <>
-            <div className='flex flex-row gap-x-[12px] h-[48px] items-center'>
+            <div className='ps-[10px] flex flex-row gap-x-[12px] h-[48px] items-center'>
                 <img src={icon} alt={title} />
                 <span>{title}</span>
             </div>
