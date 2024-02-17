18f8c2bc3d587863d616945c867d4f01890427a9
jafarmh <jafar.m@yahoo.com>
2024-02-17 22:25:52 +0330

    add img,icon to header

--- app/src/layout/Header.tsx
+++ app/src/layout/Header.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import avatar from '../asset/img/avatar/Ellipse 270.png'
+import notification from '../asset/img/icon/notification.svg'
 
 const Header = ({showHideSideBar}:{showHideSideBar:Function}) => {
     return (
         <>
-            <div className="h-[80px]   flex flex-row text-[--white]">
+            <div className="h-[80px]   flex flex-row text-[--white] items-center">
                 <div className='cursor-pointer px-1 z-20 ' onClick={()=>showHideSideBar()}><small>click hear</small></div>
-                <div className=' text-xl'>
+                <div className=' text-xl flex-grow'>
                     <b>
                     Invoices
                     </b>
+                </div>
+                <div className="flex flex-row items-center gap-x-4">
+                    <div>
+                        <img src={notification} alt="notification" />
+                    </div>
+                    <div className='flex flex-row gap-2 items-center'>
+                        <img src={avatar} alt="avatar" />
+                        <span>profile</span>
+                    </div>
                 </div>
             </div>
         </>
